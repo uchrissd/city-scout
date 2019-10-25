f1b89e6fbbcee2cae6027c3f5d96c2777b3713c8
uchrissd <uchrissd@protonmail.com>
2019-10-24 19:50:07 -0700

    deleted extra file and added main script to sript folder

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 var apiKEY = "9434dac94bff4079b3e8ae867f65cdda";
+console.log(apiKEY);
 var queryURL = "https://openexchangerates.org/api/latest.json?app_id=" + apiKEY;
 console.log(queryURL);
 
@@ -6,5 +7,5 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-  console.log(response);
+  console.log("this is the response:" + response);
 });
