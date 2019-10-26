a570296914668acccb399745b1f47ad6149b9615
uchrissd <uchrissd@protonmail.com>
2019-10-26 11:55:14 -0700

    wrote code to render continents

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//API call for currency exchange
 var apiKEY = "9434dac94bff4079b3e8ae867f65cdda";
 console.log(apiKEY);
 var queryURL = "https://openexchangerates.org/api/latest.json?app_id=" + apiKEY;
@@ -7,5 +8,36 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-  console.log("this is the response:" + response);
+  console.log(response);
 });
+
+//API call for continents
+var teleportAPIkey = "";
+var continentQueryURL = "https://api.teleport.org/api/continents/";
+
+$.ajax({
+  url: continentQueryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+//
+
+function RenderContinenets() {
+  var contArray = [
+    "Africa",
+    "Asia",
+    "Antartica",
+    "Australia",
+    "Europe",
+    "North America",
+    "South America"
+  ];
+  for (var i = 0; i < contArray.length; i++) {
+    console.log(i);
+    var cont = $("<p>" + contArray[i] + "</p>");
+    $("#cont-div").append(cont);
+  }
+}
+RenderContinenets();
