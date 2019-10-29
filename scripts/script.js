ac26fe24a4a1684793fc1b870e9c9ff7d0041690
uchrissd <uchrissd@protonmail.com>
2019-10-28 17:52:47 -0700

    wrote two api calls within a function to get us the facts of a specific city

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ console.log(queryURL);
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function (response) {
+}).then(function(response) {
   console.log(response);
 });
 
@@ -18,40 +18,85 @@ var continentQueryURL = "https://api.teleport.org/api/continents/";
 $.ajax({
   url: continentQueryURL,
   method: "GET"
-}).then(function (response) {
+}).then(function(response) {
   console.log(response);
 });
 
 //
 
-var continentsObj = { AF: 'Africa', AN: 'Antarctica', AS: 'Asia', EU: 'Europe', NA: 'North America', OC: 'Australia', SA: 'South America' }
+var continentsObj = {
+  AF: "Africa",
+  AN: "Antarctica",
+  AS: "Asia",
+  EU: "Europe",
+  NA: "North America",
+  OC: "Australia",
+  SA: "South America"
+};
 console.log(Object.keys(continentsObj));
 
 function RenderContinenets() {
-  var contArray = Object.values(continentsObj)
-  console.log(contArray)
-  var contKeys = Object.keys(continentsObj)
-  console.log(contKeys)
+  var contArray = Object.values(continentsObj);
+  console.log(contArray);
+  var contKeys = Object.keys(continentsObj);
+  console.log(contKeys);
   for (var continentCode in continentsObj) {
-    console.log("code", continentCode)
-    console.log("continent", continentsObj[continentCode])
-    var contList = $('<ul>')
+    console.log("code", continentCode);
+    console.log("continent", continentsObj[continentCode]);
+    var contList = $("<ul>");
     $("#continent").append(contList);
-    var cont = $("<li>" + continentsObj[continentCode] + "</li>").attr('data-contGeoName', continentCode)
+    var cont = $("<li>" + continentsObj[continentCode] + "</li>").attr(
+      "data-contGeoName",
+      continentCode
+    );
     $("#continent").append(cont);
   }
 }
 RenderContinenets();
-$("li").on("click", function (event) {
-  console.log(event.target.attr('data-geoname'))
-  console.log(event)
-})
+$("li").on("click", function(event) {
+  console.log(event.target.attr("data-geoname"));
+  console.log(event);
+});
 
 //JQuery get variables
 var startContainer = $("#start-container");
 var dropDownContainer = $("#dropdown-container");
 
-$(".start-btn").on("click", function () {
-  startContainer.attr("style","display:none");
-  dropDownContainer.attr("style","display:block");
-})
+$(".start-btn").on("click", function() {
+  startContainer.attr("style", "display:none");
+  dropDownContainer.attr("style", "display:block");
+});
+
+//Ajax call for city search
+// var citySearch = "https://api.teleport.org/api/cities/?search=london";
+
+// $.ajax({
+//   url: citySearch,
+//   method: "GET"
+// }).then(function(cityResponse) {
+//   console.log("This is a city search: ", cityResponse);
+// });
+
+function cityFacts() {
+  var citySearch = "https://api.teleport.org/api/cities/?search=london";
+
+  $.ajax({
+    url: citySearch,
+    method: "GET"
+  }).then(function(cityResponse) {
+    var cityGeoId =
+      cityResponse._embedded["city:search-results"][0]._links["city:item"].href;
+    console.log("this is the city id link: ", cityGeoId);
+
+    var cityFactsURL = cityGeoId;
+
+    $.ajax({
+      url: cityFactsURL,
+      method: "GET"
+    }).then(function(cityFactsResponse) {
+      var cityFacts = cityFactsResponse;
+      console.log("this is the city facts: ", cityFacts);
+    });
+  });
+}
+cityFacts();
